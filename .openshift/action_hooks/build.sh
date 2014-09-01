ef3ada5609edafba4af59dbdb708f4cde8b95b11
Puteulanus <mj19941230@gmail.com>
2014-09-01 11:49:48 +0800

    初始化

---
+++ .openshift/action_hooks/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# 下载签到助手
+cd ${OPENSHIFT_REPO_DIR}
+curl -s http://git.oschina.net/kenvix/Tieba-Cloud-Sign/repository/archive?ref=master > Tieba-sign.zip
+# 展开
+unzip Tieba-sign.zip
+cp Tieba-Cloud-Sign/* ./
+rm -rf Tieba-sign.zip
