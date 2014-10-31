49bb9e2d879d8f38cfea00463e108eadc65eec93
Puteulanus <mj@puteulanus.com>
2014-10-31 16:50:57 +0800

    增强稳定性

--- .openshift/cron/daily/online.sh
+++ .openshift/cron/daily/online.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# 获取测速节点
-content=`curl -s 'http://api.ccindex.cn/ccindex.api?cmd=4&subcmd=2'`
-# 分割节点IP及地区
-ip=`echo $content|grep -oP '(?<=\"url\":\")(\d{1,3}\.){3}\d{1,3}'`
-ips=($ip)
-place=`echo $content|grep -oP '(?<=\"province\":\")[A-Z]+'`
-places=($place)
-# 开始测速
-i=0
-for tmp in ${ips[@]}
-do
-curl -s -m 10 "http://api.ccindex.cn/ccindex.api?cmd=4&subcmd=3&url=https://${OPENSHIFT_APP_DNS}&ip=${ips[i]}&province=${places[i]}"
-i=`expr $i + 1`
-done
+url=${OPENSHIFT_APP_DNS}
+# 获取验证字符
+content=`curl -sd "url=$url&checkAll=all&dummy=" 'http://host-tracker.com/check_page/'`
+num=`echo $content|grep -oP '(?<=dt!=)\d+'`
+hh=`echo $content|grep -oP "(?<=dt=$num value=\')[^\']+"`
+# 发送测速请求
+result=`curl -svd "url=$url&hh=$hh" 'http://host-tracker.com/check_page/' 2>&1 >/dev/null | grep -oP '(?<=Location: )[^\s]+'`
